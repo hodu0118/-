0422a807ed24a74b7269dc6c850971e48d077155
hodu0118 <taeorange79@gmail.com>
2026-04-21 15:34:27 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// 부드러운 스크롤
-document.querySelectorAll('nav a').forEach(link=>{
-link.addEventListener('click', function(e){
-e.preventDefault()
-document.querySelector(this.getAttribute('href'))
-.scrollIntoView({behavior:'smooth'})
-})
-})
